e8a6ac150993878ddff69921e7f318692a119623
CliveIMPISA <augustinclive@hotmail.com>
2015-01-04 17:13:57 +0800

    added routes

---
+++ incomes_updater_worker.rb
@@ -0,0 +1,18 @@
+require 'json'
+require 'httparty'
+
+API_BASE_URI ="http://nbapaydynamo.herokuapp.com/"
+API_VER = '/api/v1/'
+
+def api_url(resource)
+  URI.join(API_BASE_URI, API_VER, resource).to_s
+end
+
+puts "Worker started"
+puts "My task_id is #{{@iron_task_id}}"
+puts "Paramters: '#{params}'"
+
+saved_incomes = HTTParty.get api_url("incomes")
+saved_incomes.each do |income|
+  incomes_url = api_url("incomes/#{incomes['id']}")
+end
