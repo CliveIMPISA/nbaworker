2fa8b22846d6f803cbef57fa1c4ae2517da4d4f6
CliveIMPISA <augustinclive@hotmail.com>
2015-01-07 21:48:03 +0800

    trying to save results

--- incomes_updater_worker.rb
+++ incomes_updater_worker.rb
@@ -12,9 +12,23 @@ puts "Worker started"
 puts "My task_id is #{@iron_task_id}"
 puts "Parameters: '#{params}'"
 
-saved_incomes = HTTParty.get api_url("incomes")
-saved_incomes.each do |income|
-  incomes_url = api_url("incomes/#{income['id']}")
-  results =HTTParty.get incomes_url
-  puts "Updated: #{income['id']}"
+saved_singles = HTTParty.get api_url("single")
+saved_singles.each do |single|
+  single_url = api_url("single/#{single['id']}")
+  results = HTTParty.get single_url
+  puts "Updated: #{single['id']}"
+end
+
+saved_doubles = HTTParty.get api_url("double")
+saved_doubles.each do |double|
+  double_url = api_url("double/#{double['id']}")
+  results = HTTParty.get double_url
+  puts "Updated: #{double['id']}"
+end
+
+saved_results = HTTParty.get api_url("result")
+saved_results.each do |result|
+  result_url = api_url("single/#{result['id']}")
+  results = HTTParty.get result_url
+  puts "Updated: #{result['id']}"  
 end
